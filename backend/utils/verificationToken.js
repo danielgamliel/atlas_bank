7c9f0758aa4ffcc9fe424597feb064f728ac4f29
daniel <danielgamlieli1@gmail.com>
2026-02-05 12:19:54 +0200

    varify failed ans success

--- backend/utils/verificationToken.js
+++ backend/utils/verificationToken.js
@@ -3,7 +3,7 @@ import crypto from "crypto";
 
 export function createVerificationToken() {
   const token = crypto.randomBytes(32).toString("hex"); // 64 chars
-  const expiresAt = new Date(Date.now() + 20 * 60 * 1000); // 20 min
+  const expiresAt = new Date(Date.now() + 200 * 60 * 1000); // 200 min
 
   return { token, expiresAt };
 }
